c32b10989e3a59c7fbdf009637f9f3ca887ec1d5
JackHallisy <39491444+JackHallisy@users.noreply.github.com>
2018-10-25 18:31:26 -0400

    Add files via upload

---
+++ Controllers/CollectionSortController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CollectionSortEmpty.Controllers
+{
+    public class CollectionSortController : Controller
+    {
+        // GET: CollectionSort
+        public ActionResult Index()
+        {
+            List<int> list = new List<int>();
+            Random rand = new Random();
+
+            for (int ctr = 0; ctr <= 4; ctr++)
+            {
+                list.Add(rand.Next(0, 100));
+                list.Sort();
+
+                ViewBag.List = list;
+            }
+            return View();
+
+        }
+    }
+}
